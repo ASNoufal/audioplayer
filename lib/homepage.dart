2b5cfc6ad66cdbbbc2237e7a6a88d36cdad1d767
noufal <naufal202@gmail.com>
2023-06-30 17:34:49 +0530

    songs ui added and some ui updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
         children: [
           Row(
             children: [
-              Padding(
+              const Padding(
                 padding: EdgeInsets.all(15.0),
                 child: Text(
                   "Good morning",
@@ -20,38 +20,71 @@ class HomePage extends StatelessWidget {
                   style: kdash,
                 ),
               ),
-              Spacer(),
+              const Spacer(),
               IconButton(
                   onPressed: () {},
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.favorite_outline_outlined,
                     color: Colors.white,
                   )),
               IconButton(
                   onPressed: () {},
-                  icon: Icon(
+                  icon: const Icon(
                     Icons.blur_on,
                     color: Colors.white,
                   ))
             ],
           ),
           ListTile(
-            leading: CircleAvatar(),
-            title: Text(
+            leading: const CircleAvatar(),
+            title: const Text(
               "NAUFAL AS",
               style: TextStyle(
                   color: Colors.white38, fontSize: 10, letterSpacing: 1),
             ),
-            subtitle: Text(
+            subtitle: const Text(
               "Listen again",
               style: kdash,
             ),
             trailing: TextButton(
               onPressed: () {},
-              child: Text(
+              child: const Text(
                 "more",
                 style: TextStyle(color: Colors.white38),
               ),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 4,
+              childAspectRatio: 1.5,
+              scrollDirection: Axis.horizontal,
+              children: [
+                InkWell(
+                  child: Image(
+                    image: AssetImage("assets/songsSS/chayapattu.jpeg"),
+                    width: 40,
+                    height: 40,
+                  ),
+                ),
+                InkWell(
+                  child: Image.asset(
+                      "assets/songsSS/ee39c9d3-177c-4045-b0b2-c1e3df597370_1024.jpeg"),
+                ),
+                InkWell(child: Image.asset("assets/songsSS/letme.jpeg")),
+                InkWell(child: Image.asset("assets/songsSS/mocking bird.jpeg")),
+                InkWell(child: Image.asset("assets/songsSS/rahmatun.jpeg")),
+                InkWell(
+                  child: Image.asset(
+                      "assets/songsSS/Sulaikha-Manzil-Malayalam-2023-20230509171010-500x500.jpeg"),
+                ),
+                InkWell(
+                    child:
+                        Image.asset("assets/songsSS/urukumen azhalinu.jpeg")),
+                InkWell(
+                    child: Image.asset("assets/songsSS/We_Are_One_cover.png")),
+              ],
             ),
           ),
 
