4d4c9e407b60431318b4e1bb3a4b661712ce42d9
noufal <naufal202@gmail.com>
2023-07-04 15:06:37 +0530

    settings ui updated

--- lib/settings.dart
+++ lib/settings.dart
@@ -5,6 +5,47 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("settings");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text("Notification"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Nick Name"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Terms and Condition"),
+            subtitle: Text("All the stuff you need to know"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Privacy Policy"),
+            subtitle: Text("Important for both of us"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Share the App"),
+            subtitle: Text("shareeee....!!!"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("Third party Application"),
+            subtitle: Text("......!!!!"),
+            trailing: Icon(Icons.arrow_forward),
+          ),
+          ListTile(
+            title: Text("About"),
+            subtitle: Text("About the App"),
+            trailing: Icon(Icons.arrow_forward),
+          )
+        ],
+      ),
+    );
   }
 }
